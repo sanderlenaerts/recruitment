b53067a5109e491ace371310dbdcb89b7a3443f9
Sander Lenaerts <sander.lenaerts@student.ucll.be>
2017-06-09 11:52:07 +1200

    Updated build process for production

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,12 +3,15 @@ const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const AppCachePlugin = require('appcache-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Directory where the compiled will go: copy everything to dist folder
 const DIST_DIR = path.resolve(__dirname, "dist")
 
 // Where to find the entrance source code files
 const SRC_DIR = path.resolve(__dirname, "src");
+
+const PUB_PATH = '/app/'
 
 const config = {
     // Where to look for entries
@@ -25,7 +28,7 @@ const config = {
         path: path.resolve(__dirname, DIST_DIR, 'app'),
         filename: '[name].bundle.js',
         // To tell webpack dev server where our product is located
-        publicPath: '/app/'
+        publicPath: PUB_PATH
     },
     module: {
         rules: [
@@ -33,11 +36,24 @@ const config = {
                 test: /\.js$/,
                 include: SRC_DIR,
                 use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['react', 'es2015', 'stage-2']
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['react', 'es2015', 'stage-2']
+                        }
                     }
-                }]
+                ]
+            },
+            {
+                test: /\.(js|html|css|sass)$/,
+                include: SRC_DIR,
+                use: [{
+                    loader: 'string-replace-loader',
+                    query: {
+                        search: '/app/',
+                        replace: PUB_PATH,
+                        flags: 'g'
+                    }
+                }]     
             },
             {
                 test: /\.(sass|scss)$/,
@@ -49,18 +65,18 @@ const config = {
             {
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/,
                 loader: 'url-loader?limit=100000'
-            }
+            },
         ]
     },
     plugins: [
-        new ExtractTextPlugin('./assets/css/styles.css'),
+        new ExtractTextPlugin('assets/css/styles.css'),
         
         new CopyWebpackPlugin(
             [
-                 { 
-                    from: SRC_DIR + '/index.html', 
-                    to: DIST_DIR  
-                 },
+                //  { 
+                //     from: SRC_DIR + '/index.html', 
+                //     to: DIST_DIR  
+                //  },
                  {
                      from: './app/assets/images',
                      to: './assets/images'
@@ -69,16 +85,20 @@ const config = {
             ]
         ),
 
-        new AppCachePlugin({
-            cache: [''],
-            network: null,
-            settings: ['prefer-online'],
+         new AppCachePlugin({
             output: 'my-manifest.appcache'
         }),
          new webpack.DefinePlugin({
             'process.env' : {
                 'build': JSON.stringify('www')
             }
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Recruitment App',
+            template: './index.ejs',
+            favicon: './app/assets/images/favicon.ico',
+            filename: '../index.html',
+            manifest: PUB_PATH
         })
     ]
 }
